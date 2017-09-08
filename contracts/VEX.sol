7cdd31167cc6ec3f8cc7fdbc8880ca2214b66cf1
Travis Hoppe <travis.hoppe@gmail.com>
2017-09-08 17:05:10 -0400

    Actually using the blockchain now for computations

--- contracts/VEX.sol
+++ contracts/VEX.sol
@@ -2,16 +2,23 @@ pragma solidity ^0.4.4;
 
 contract VEX {
 
-  function add(int x, int y) public returns (int) {
+  event result(int _value);
+
+  function add(int x, int y) constant public returns (int) {
     return x+y;
   }
 
-  function subtract(int x, int y) public returns (int) {
+  function subtract(int x, int y) constant public returns (int) {
     return x-y;
   }
 
-  function multiply(int x, int y) public returns (int) {
+  function multiply(int x, int y) constant public returns (int) {
     return x*y;
   }
+
+  function network_add(int x, int y) public {
+    result(add(x,y));
+  }
   
+
 }
--- test/VEX.js
+++ test/VEX.js
@@ -1,5 +1,33 @@
 var VEX = artifacts.require("./VEX.sol");
 
+function test_constant_function(func_name, a, b, expected_result) {
+    
+    return VEX.deployed().then(function(instance) {
+	return instance[func_name].call(a,b);
+    }).then(function(result) {
+	console.log(func_name, a, b, "= " + result.toNumber());
+	assert.equal(result.toNumber(), expected_result);
+    });
+    
+};
+
+function test_network_function(func_name, a, b, expected_result) {
+
+    return VEX.deployed().then(function(instance) {
+	return instance[func_name](a,b);
+    }).then(function(result) {
+	log = result.logs[0]
+	val = log.args._value;
+	console.log(a + "+" + b + "=" + val.toNumber());
+	console.log("Mined on blockNumber", log.blockNumber);
+	console.log("Gas used: ", result.receipt.gasUsed);
+	assert.equal(a+b, val.toNumber());
+    });
+};
+
+
+
+
 contract('VEX', function(accounts) {
 
     var a = 5;
@@ -8,32 +36,22 @@ contract('VEX', function(accounts) {
     it("Say hello", function() {
 	console.log("hello");
     });
+    
 
     it("Add two numbers", function() {
-	return VEX.deployed().then(function(instance) {
-	    return instance.add.call(a,b);
-	}).then(function(result) {
-	    assert.equal(a+b, result.toNumber());
-	    console.log(a + "+" + b + "=" + result.toNumber());	    
-	});
+	test_constant_function("add", a, b, a+b);
     });
 
     it("Subtract two numbers", function() {
-	return VEX.deployed().then(function(instance) {
-	    return instance.subtract.call(a,b);
-	}).then(function(result) {
-	    assert.equal(a-b, result.toNumber());
-	    console.log(a + "-" + b + "=" + result.toNumber());	    
-	});
+	test_constant_function("subtract", a, b, a-b);
     });
 
     it("Multiply two numbers", function() {
-	return VEX.deployed().then(function(instance) {
-	    return instance.multiply.call(a,b);
-	}).then(function(result) {
-	    assert.equal(a*b, result.toNumber());
-	    console.log(a + "*" + b + "=" + result.toNumber());	    
-	});
+	test_constant_function("multiply", a, b, a*b);
     });
-    
+
+    it("Add two numbers on the network", function() {
+	test_network_function("network_add", a, b, a+b);
+    });
+
 });
